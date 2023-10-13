2a8d246a26394e6806afc811e03e3866a29d1230
polubis <polubis@wp.pl>
2023-10-13 08:36:47 +0200

    build(app): add test code environment

--- src/design-system/button.tsx
+++ src/design-system/button.tsx
@@ -1,15 +1,18 @@
-import React, { type ReactNode } from 'react';
+import React from 'react';
+import c from 'classnames';
 
-interface ButtonProps {
-  children: ReactNode;
-}
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {}
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return (
-    <button className="bg-gray-300 dark:bg-red-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-      {children}
-    </button>
+const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+  const classes = c(
+    className,
+    `bg-gray-300 dark:bg-red-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center`,
   );
+  return <button className={classes} {...props} />;
 };
 
 export { Button };
